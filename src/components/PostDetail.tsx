fb000ca325e02921b60b13371bcc5286bd031dfb
LKJ970524 <dlrbwjd7096@naver.com>
2024-03-01 23:13:00 +0900

    feat: 프로젝트 컴포넌트 작업(프로필 및 게시글 상세)

---
+++ src/components/PostDetail.tsx
@@ -0,0 +1,13 @@
+import PostDetail from "../pages/posts/detail";
+import Header from "./Header";
+import Footer from "./Footer";
+
+export default function PostPage() {
+  return (
+    <>
+      <Header />
+      <PostDetail />
+      <Footer />
+    </>
+  );
+}
--- src/pages/posts/detail.tsx
+++ src/pages/posts/detail.tsx
@@ -1,5 +1,37 @@
-export default function PostDetail () {
+export default function PostDetail() {
   return (
-    <h1>Post Detail Page</h1>
-  )
+    <>
+      <div className="post__detail">
+        <div className="post__box">
+          <div className="post__title">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+          </div>
+          <div className="post__profile-box">
+            <div className="post__profile" />
+            <div className="post__author-name">이규정</div>
+            <div className="post__date">2024.03.01 금요일</div>
+          </div>
+          <div className="post__utils-box">
+            <div className="post__delete">삭제</div>
+            <div className="post__edit">수정</div>
+          </div>
+          <div className="post__text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Pellentesque ac aliquam felis. Phasellus lobortis varius malesuada.
+            Phasellus vestibulum mauris ac urna sagittis, eu sodales mauris
+            rutrum. Quisque ut imperdiet eros. Maecenas imperdiet euismod leo ac
+            rutrum. In in arcu vel libero posuere scelerisque. Maecenas non
+            ligula sapien. Maecenas aliquam egestas massa, non lacinia est
+            iaculis finibus. Aliquam justo sem, congue et porttitor et, commodo
+            sit amet risus. Curabitur gravida ultricies efficitur. Sed feugiat
+            nisl nec neque egestas, ac gravida sem pretium. Sed ultricies
+            bibendum arcu, et aliquam risus condimentum eu. Suspendisse blandit
+            orci vel commodo pharetra. Integer in lorem nec ipsum semper
+            tincidunt a sed diam. Pellentesque neque metus, consequat at feugiat
+            nec, tincidunt at felis.
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
