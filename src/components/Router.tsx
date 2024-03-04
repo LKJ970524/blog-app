4f376563351e8cb09094db03ff25ec1859ccb080
LKJ970524 <dlrbwjd7096@naver.com>
2024-03-04 23:07:26 +0900

    feat: Route 적용(로그인과 비로그인 상태 구분)

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -7,22 +7,34 @@ import PostEdit from "pages/posts/edit";
 import Profile from "pages/profile";
 import Login from "pages/login";
 import SignUp from "pages/signup";
+import { useState } from "react";
 
 export default function Router() {
+  // firebase Auth가 인증되었으면 true로 변경해주는 로직 추가
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/posts" element={<PostList />} />
-        <Route path="/posts/:id" element={<PostDetail />} />
-        <Route path="/posts/new" element={<PostNew />} />
-        <Route path="/posts/edit/:id" element={<PostEdit />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="*" element={<Navigate replace to='/' />} />
+        {isAuthenticated ? (
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/posts" element={<PostList />} />
+            <Route path="/posts/:id" element={<PostDetail />} />
+            <Route path="/posts/new" element={<PostNew />} />
+            <Route path="/posts/edit/:id" element={<PostEdit />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="*" element={<Navigate replace to="/" />} />
+          </>
+        ) : (
+          <>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="*" element={<Login />} />
+          </>
+        )}
       </Routes>
     </>
   );
 }
-
