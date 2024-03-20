f3a1c639bd3a55dad8918f1789c6b10e8d66b745
LKJ970524 <dlrbwjd7096@naver.com>
2024-03-20 21:35:16 +0900

    feat: 댓글 UI 작업

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -5,6 +5,7 @@ import { doc, getDoc, deleteDoc } from "firebase/firestore";
 import { db } from "firebaseAPP";
 import Loader from "./Loader";
 import { toast } from "react-toastify";
+import Comments from "./Comments";
 
 export default function PostDetail() {
   const [post, setPost] = useState<PostProps | null>(null);
@@ -37,32 +38,35 @@ export default function PostDetail() {
     <>
       <div className="post__detail">
         {post ? (
-          <div className="post__box">
-            <div className="post__title">{post?.title}</div>
-            <div className="post__profile-box">
-              <div className="post__profile" />
-              <div className="post__author-name">{post?.email}</div>
-              <div className="post__date">{post?.createdAt}</div>
-            </div>
-            <div className="post__utils-box">
-              {post?.category && (
-                <div className="post__category">{post?.category}</div>
-              )}
-              <div
-                className="post__delete"
-                role="presentation"
-                onClick={handleDelete}
-              >
-                삭제
+          <>
+            <div className="post__box">
+              <div className="post__title">{post?.title}</div>
+              <div className="post__profile-box">
+                <div className="post__profile" />
+                <div className="post__author-name">{post?.email}</div>
+                <div className="post__date">{post?.createdAt}</div>
               </div>
-              <Link to={`/posts/edit/${post?.id}`} className="post__edit">
-                수정
-              </Link>
+              <div className="post__utils-box">
+                {post?.category && (
+                  <div className="post__category">{post?.category}</div>
+                )}
+                <div
+                  className="post__delete"
+                  role="presentation"
+                  onClick={handleDelete}
+                >
+                  삭제
+                </div>
+                <Link to={`/posts/edit/${post?.id}`} className="post__edit">
+                  수정
+                </Link>
+              </div>
+              <div className="post__text post__text--pre-wrap">
+                {post?.content}
+              </div>
             </div>
-            <div className="post__text post__text--pre-wrap">
-              {post?.content}
-            </div>
-          </div>
+            <Comments />
+          </>
         ) : (
           <Loader />
         )}
